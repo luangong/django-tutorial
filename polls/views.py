647210a25d07e8c0f631d9fbd88ef7fc191bea50
Luan Gong <luan.gong@gmail.com>
2016-10-24 22:43:14 -0700

    Use Django's template system to separate design from Python code

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = '<br>'.join(q.question_text for q in latest_question_list)
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    template = loader.get_template('polls/detail.html')
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    template = loader.get_template('polls/results.html')
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return HttpResponse(template.render(context, request))
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    template = loader.get_template('polls/vote.html')
+    question = Question.objects.get(pk=question_id)
+    context = {'question': question}
+    return HttpResponse(template.render(context, request))
