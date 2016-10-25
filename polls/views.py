337b4872442e89e2cb4131c8afc733b1a409ad5f
Luan Gong <luan.gong@gmail.com>
2016-10-24 22:47:33 -0700

    Replace the template and the loader with the shortcut render() function
    It's a very common idiom to load a template, fill a context and return
    an HttpResponse object with the result of the rendered template.  Django
    provides a shortcut render() function to do this.
    
    The render() function takes the request object as its first argument, a
    template name as its second argument and a dictionary as its optional
    third argument.  It returns an HttpResponse object of the given template
    rendered with the given context.

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,19 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    template = loader.get_template('polls/detail.html')
     question = Question.objects.get(pk=question_id)
-    context = {'question': question}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    template = loader.get_template('polls/results.html')
     question = Question.objects.get(pk=question_id)
-    context = {'question': question}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    template = loader.get_template('polls/vote.html')
     question = Question.objects.get(pk=question_id)
-    context = {'question': question}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/vote.html', {'question': question})
